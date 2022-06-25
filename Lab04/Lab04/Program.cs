bc24b222eca0125bb901bc1179bc6bad248418ee
MiguelJerome <sharkmaycry@gmail.com>
2022-06-25 15:34:32 -0400

    updated uml rectangle

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -1,2 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class Lampe
+{
+
+    private byte _etat;
+
+    public void Lampe()
+    {
+
+    }
+
+    public void Eteindre()
+    {
+
+    }
+
+    public void Allumer
+    {
+
+    }
+
+    public void Etat()
+    {
+
+    }
+
+    public void Afficher()
+    {
+
+    }
+
+    enum Couleur 
+    { 
+        Jaune
+    }
+}
+
+
+
+
+
+
+
+
+
+
