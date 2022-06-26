5af210efdf2e6ead6fb34778b665173041775654
MiguelJerome <sharkmaycry@gmail.com>
2022-06-26 16:37:19 -0400

    create Employe class

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -1,34 +1,34 @@
-﻿//// See https://aka.ms/new-console-template for more information
-////Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
 
-//var lampe11 = new Lampe();
-//lampe11.Eteindre();
-//var lampe13 = new Lampe(true);
-//lampe13.Allumer();
-//var lampe12 = new Lampe(true, "Rouge");
-//Console.WriteLine($"Etat Lampe 12 : {lampe12.Etat()}");
+var lampe11 = new Lampe();
+lampe11.Eteindre();
+var lampe13 = new Lampe(true);
+lampe13.Allumer();
+var lampe12 = new Lampe(true, "Rouge");
+Console.WriteLine($"Etat Lampe 12 : {lampe12.Etat()}");
 
-////afficher les lampes 11, 12, 13
-//Console.WriteLine();
-//Console.WriteLine();
+//afficher les lampes 11, 12, 13
+Console.WriteLine();
+Console.WriteLine();
 
-//Console.WriteLine($"Afficher les lampes 11, 12, 13");
-//Console.WriteLine();
+Console.WriteLine($"Afficher les lampes 11, 12, 13");
+Console.WriteLine();
 
-//Console.WriteLine($"***Lampe 11***");
-//lampe11.Afficher();
-//Console.WriteLine();
+Console.WriteLine($"***Lampe 11***");
+lampe11.Afficher();
+Console.WriteLine();
 
-//Console.WriteLine($"***Lampe 12***");
-//lampe12.Afficher();
-//Console.WriteLine();
+Console.WriteLine($"***Lampe 12***");
+lampe12.Afficher();
+Console.WriteLine();
 
-//Console.WriteLine($"***Lampe 13***");
-//lampe13.Afficher();
-//Console.WriteLine();
+Console.WriteLine($"***Lampe 13***");
+lampe13.Afficher();
+Console.WriteLine();
 
-//var employe = new Employe();
-//employe.Afficher();
+var employe = new Employe();
+employe.Afficher();
 
 var emp1 = new Employe("bouhiel", "mohamed  salah", 4500, 2005, "123 Rue Charlemagne, Gatineau, A1B2C3, Canada");
 var emp2 = new Employe("duval", "Eric", 2500, 2015);
